2b56b846b54df08a15c4ce0da2664aac632f1ed0
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-09-30 17:16:21 -0400

    created game functions.

--- game.js
+++ game.js
@@ -1,5 +1,4 @@
 // define variables
-
 let game;
 let player;
 let platforms;
@@ -15,13 +14,11 @@ let lives = 5;
 let level;
 
 // barrier randomizer
-
 function randomItems() {
   let items = Math.floor(Math.random() * 500) - 1;
 }
 
 // add collectable items to the game
-
 function addItems() {
   items = game.add.physicsGroup();
   createItem(375, 400, 'coin');
@@ -39,13 +36,11 @@ function addItems() {
 }
 
 // add platforms
-
 function addPlatforms() {
   platforms = game.add.physicsGroup();
 }
 
 // create a single animated item
-
 function createItem(left, top, image) {
   let item = items.create(left, top, image);
   item.animations.add('spin');
@@ -53,8 +48,7 @@ function createItem(left, top, image) {
 }
 
 // when the player collects an item on the screen
-
-itemsPlusHandler(player, itemsPlus) => {
+_itemsPlus(player, itemsPlus) => {
   itemPlus.kill();
   switch (itemsPlus) {
     case 'honey': currentScore += 10;
@@ -64,7 +58,7 @@ itemsPlusHandler(player, itemsPlus) => {
   }
 }
 
-itemsMinusHandler(player, itemsMinus) => {
+_itemsMinus(player, itemsMinus) => {
   itemsMinus.kill();
   switch (itemsMinus) {
     case 'poison': lives -= 1;
@@ -72,4 +66,34 @@ itemsMinusHandler(player, itemsMinus) => {
     case 'mist': lives -= 2;
     [break;]
   }
+}
+
+// when the player collects the badge
+_badge() => {
+  badge.kill();
+  won = true;
+}
+
+// setup game when the web page loads
+window.onload = function () {
+  game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
+}
+
+// before the game begins
+function preload() {
+  game.stage.backgroundColor = '#5db1ad';
+}
+
+//load images
+
+//load spritesheets
+
+//initial game set up
+function create() {
+  player = game.add.sprite(50, 600, 'player');
+  player.animations.add('walk');
+  player.anchor.setTo(0.5, 1);
+  game.physics.arcade.enable(player);
+  player.body.collideWorldBounds = true;
+  player.body.gravity.y = 800;
 }
