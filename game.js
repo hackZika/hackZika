53177405cee77f5bf8abdb2c8b97bc42f0c962a3
wwwhatley <30512540+wwwhatley@users.noreply.github.com>
2017-09-30 23:14:53 -0400

    configured avatar movement, fixed itemHandler function, repaired game handlers, added score and life tracker, and loaded new spritesheets.

--- game.js
+++ game.js
@@ -8,8 +8,7 @@ let jumpButton;
 let text;
 let winningMessage;
 let won = false;
-let startingScore = 0;
-let currentScore;
+let currentScore = 0;
 let lives = 5;
 let level;
 
@@ -18,11 +17,11 @@ let level;
 //   let items = Math.floor(Math.random() * 500) - 1;
 // }
 
-// add collectable items to the game
+// add collectable
 function addItems() {
   items = game.add.physicsGroup();
   createItem(375, 400, 'coin');
-  createItem(575, 500, 'coin');
+  createItem(575, 500, 'poison');
 }
 
 // add platforms
@@ -37,29 +36,39 @@ function createItem(left, top, image) {
   item.animations.play('spin', 10, true);
 }
 
-// when the player collects an item on the screen
-function _itemsPlus(player, itemsPlus) {
-  itemsPlus.kill();
-  switch (itemsPlus) {
-    case 'honey': currentScore += 10;
-    break;
-    case 'water': currentScore += 20;
-    break;
-  }
+// create badge to go to next level
+
+function createBadge() {
+  badges = game.add.physicsGroup();
+  let badge = badges.create(750, 400, 'badge');
+  badge.animations.add('spin');
+  badge.animations.play('spin', 10, true);
 }
 
-function _itemsMinus(player, itemsMinus) {
-  itemsMinus.kill();
-  switch (itemsMinus) {
+// when the player collects an item on the screen
+
+function itemHandler(player, item) {
+  item.kill();
+  switch (item.key) {
     case 'poison': lives -= 1;
     break;
     case 'mist': lives -= 2;
+    break;
+    case 'window': lives -= 3;
+    break;
+    case 'fish': lives = lives - lives;
+    break;
+    case 'coin': currentScore += 25;
+    break;
+    case 'honey': currentScore += 50;
+    break;
+    case 'heart': lives += 1;
     break;
   }
 }
 
 // when the player collects the badge
-function _badge() {
+function badgeHandler(player, badge) {
   badge.kill();
   won = true;
 }
@@ -67,18 +76,19 @@ function _badge() {
 // setup game when the web page loads
 window.onload = function () {
   game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
-}
 
 // before the game begins
 function preload() {
   game.stage.backgroundColor = '#5db1ad';
-  game.load.spritesheet('coin', 'assets/coin.png', 36, 44);
-}
+
 
 //load images
 
 //load spritesheets
+game.load.spritesheet('coin', 'assets/coin.png', 36, 44);
+game.load.spritesheet('poison', 'assets/poison.png', 32, 32);
 
+}
 //initial game set up
 function create() {
   player = game.add.sprite(50, 600, 'player');
@@ -86,11 +96,12 @@ function create() {
   player.anchor.setTo(0.5, 1);
   game.physics.arcade.enable(player);
   player.body.collideWorldBounds = true;
-  player.body.gravity.y = 800;
+  player.body.gravity.y = 100;
 
   addItems();
+  // addPlatforms();
 
-  cursors = game.input.keyboard.createCursorsKeys();
+  cursors = game.input.keyboard.createCursorKeys();
   jumpButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   text = game.add.text(16, 16, "SCORE: " + currentScore, { font: "bold 24px Arial", fill: "white"});
   winningMessage = game.add.text(game.world.centerX, 275, "", { font: "bold 48px Arial", fill: "white"});
@@ -100,17 +111,40 @@ function create() {
 //while the game is running
 
 function update() {
-  // text.text = "SCORE: " + currentScore;
-  // game.physics.arcade.collide(player, platforms);
-  // game.physics.arcade.overlap(player, items, _itemsPlus);
-  // game.physics.arcade.overlap(player, items, _itemsMinus);
-  // game.physics.arcade.overlap(player, badges, _badge);
+  text.text = "SCORE: " + currentScore + " LIVES: " + lives ;
+  game.physics.arcade.collide(player, platforms);
+  game.physics.arcade.overlap(player, items, itemHandler);
+  game.physics.arcade.overlap(player, badges, badgeHandler);
   player.body.velocity.x = 0;
+
+  if (cursors.left.isDown) {
+    player.animations.play('walk', 10, true);
+    player.body.velocity.x = -300;
+    player.scale.x = - 1;
+  }
+  else  if (cursors.right.isDown) {
+    player.animations.play('walk', 10, true)
+    player.body.velocity.x = 300;
+    player.scale.x = 1;
+
+
+  } else {
+    player.animations.stop();
+  }
+
+  if (jumpButton.isDown && (player.body.onFloor() || player.body.touching.down)) {
+    player.body.velocity.y = -400;
+  }
+
+  if (won) {
+    winningMessage.text = "Prepare for the next level!";
+  }
+
 }
+
 
 function render() {
 
-}
+  }
 
-//left cursor
-// if (cursors.left.isDown)
+};
